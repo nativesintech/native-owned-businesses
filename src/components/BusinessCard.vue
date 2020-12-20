4ca17026876c3e1bde73d40175e625806db338b3
Nick Sahler <nicksahler@users.noreply.github.com>
2020-12-20 03:55:36 -0500

    Fix style

--- src/components/BusinessCard.vue
+++ src/components/BusinessCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col md:flex-row border border-gray-600 mb-6 shadow
+  <div class="flex flex-col overflow-hidden md:flex-row border border-gray-600 mb-6 shadow
     rounded">
     <div class="overflow-hidden rounded-t border-b border-gray-600 flex-shrink
       flex flex-row h-64 md:w-1/4 md:border-0 md:rounded-none
