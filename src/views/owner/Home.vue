a2b472d6843d057b3f8c87a8f89f7f32000bb984
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-17 15:59:06 -0600

    My businesses and add business button

--- src/views/owner/Home.vue
+++ src/views/owner/Home.vue
@@ -1,8 +1,14 @@
 <template>
-  <main>Businesses</main>
+  <main>
+    <section class="flex justify-between items-center mb-4">
+      <h1 class="font-bold text-2xl">My Businesses</h1>
+      <button type="button" class="bg-black rounded text-white px-6 py-2">Add Business</button>
+    </section>
+    <section>
+      List of Businesses
+    </section>
+  </main>
 </template>
 <script>
-export default {
-
-}
+export default { }
 </script>
