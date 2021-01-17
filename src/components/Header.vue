3aa08ef7ffa9b21b50ad6a0b9998ad2ca3236cd7
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-17 14:38:37 -0600

    Add basic login with simple validation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,9 +14,14 @@
             Native Owned Businesses
           </h1>
         </div>
-        <router-link to="/about">
-          About
-        </router-link>
+        <div class="space-x-2">
+          <router-link to="/about">
+            About
+          </router-link>
+          <router-link to="/login">
+            Login
+          </router-link>
+        </div>
       </nav>
     </header>
   </router-link>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <h1 class="text-2xl mb-4 font-bold">Login</h1>
+    <form @submit="handle_submit" >
+      <label for="email" class="text-sm text-gray-600 leading-normal mb-2">Email</label><br />
+      <input required id="email" v-model="email" name="email" class="border-2 border-gray-600 rounded text-lg p-2 mb-4" type="email" /><br/>
+      <button class="bg-black text-white px-6 py-2 rounded" type="submit">Login</button>
+    </form>
+  </main>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      email: undefined
+    }
+  },
+  methods: {
+    handle_submit (e) {
+      e.preventDefault()
+
+      // Submit Form
+    }
+  }
+}
+</script>
