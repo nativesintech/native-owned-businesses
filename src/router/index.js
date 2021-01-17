f6c4de8c36af1b67c2f45971f5e9e7f675fe0ba3
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-17 11:15:15 -0600

    Create about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,8 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  }
+  },
+  { path: '/about', name: 'about', component: About }
 ]
 
 const router = new VueRouter({
---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="pb-8">
+    <Header :is_embedded="is_embedded" />
+    <main class="space-y-4">
+      <section class="space-y-2">
+        <h1 class="text-2xl font-bold">About</h1>
+        <p class="text-base text-gray-600 leading-normal mb-6">
+          Native Owned Businesses is a curated directory of Native businesses of all kinds throughout the world.
+        </p>
+        <p class="text-base text-gray-600 leading-normal mb-6">
+          This project is an intiative of <a class="underline" target="_blank" rel="noopener noreferrer" href="https://nativesintech.org">Native in Tech</a>, a collective of Natives in technology building free and open source technology for Native communities.
+        </p>
+      </section>
+      <section class="space-y-2">
+        <h2 class="text-2xl font-bold">FAQs</h2>
+        <ul class="space-y-4">
+          <li class="font-bold">Who is considered Native?</li>
+          <p class="text-base text-gray-600 leading-normal mb-6">A Native person is someone who is a citizen of a federally recognized nation. Visit the <a class="underline" target="_blank" rel="noopener noreferrer" href="https://www.bia.gov/frequently-asked-questions">BIA FAQs</a> for more answers on this question.</p>
+          <li class="font-bold">What is a Native-owned business?</li>
+          <p class="text-base text-gray-600 leading-normal mb-6">A Native-owned businsess is a business whose owners are majority Native and whose business proclaims to be Native-owned or whose majority owners have Native citizenship.</p>
+          <li class="font-bold">I am a Native business owner. How do I sign up?</li>
+          <p class="text-base text-gray-600 leading-normal mb-6">The process for signing up is manual for now. If you are a Native-owned business then please send an email to <a class="underline" href="mailto:nob@nativesintech.org">nob@nativesintech.org</a> asking to be added to this directory.</p>
+          <li class="font-bold">I have citizenship in a nation but it is not recognized or I am a citizen of a state recognized nation. Can I still be added to the directory?</li>
+          <p class="text-base text-gray-600 leading-normal mb-6">It depends. There are nations such as the <a class="underline" target="_blank" rel="noopener noreferrer" href="https://indianlaw.org/projects/past_projects/seminole">Independent Traditional Seminole Nation</a> who do not believe in citizenship and who follow a traditional way of life. In contrast, there also exists wannabe cultures who reframe and adopt native cultures but who do not have a connection to any Native community. We will do our best to preserve the integrity of this directory and do our research on a case-by-case basis.</p>
+          <li class="font-bold">I can't find a business for x. Why don't you have it?</li>
+          <p class="text-base text-gray-600 leading-normal mb-6">In order to maintain the integrity of this directory we are adding business owners manually. It will take time for the spectrum of businesses to show up here.</p>
+        </ul>
+      </section>
+    </main>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: { Header },
+  data () {
+    return {
+      is_embedded: false
+    }
+  }
+}
+</script>
