b03527d2f4d78f6cf44ae52e87c1eccfe8e2489d
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-17 11:15:07 -0600

    Create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header
+      class="py-6 bg-white"
+      v-if="!is_embedded"
+    >
+    <nav class="flex items-center justify-between w-100">
+      <div class="flex items-center">
+        <a href="/">
+          <img
+            src="@/assets/logo.svg" alt="Logo"
+            class="flex-shrink-0 h-12 w-12"
+          >
+        </a>
+        <h1 class="text-xl sm:text-xl lg:text-4xl text-gray-900 font-bold ml-3">
+          Native Owned Businesses
+        </h1>
+      </div>
+      <a href="/about">About</a>
+    </nav>
+    </header>
+</template>
+<script>
+export default {
+  props: ['is_embedded']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,131 +1,112 @@
 <template>
-  <div class="flex flex-col text-left">
-    <div
-      class="flex py-6 bg-white items-center"
-      v-if="!is_embedded"
-    >
-      <img
-        src="@/assets/logo.svg" alt="Logo"
-        class="flex-shrink-0 h-12 w-12"
-      >
-      <h1 class="text-xl sm:text-xl lg:text-4xl text-gray-900 font-bold ml-3">
-        Native Owned Businesses
-      </h1>
-    </div>
-    <div>
-      <p class="text-base text-gray-600 leading-normal mb-6">
-        Nori grape silver beet broccoli kombu beet greens fava bean potato
-        quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil
-        turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant
-        winter purslane fennel azuki bean earthnut pea sierra leone bologi leek
-        soko chicory celtuce parsley jícama salsify.
-      </p>
-    </div>
+  <div>
+    <Header :is_embedded="is_embedded" />
+    <main>
+      <div
+        ref="hidebar"
+        :style="search_offset"
+        v-if="!$route.query.hide_search"
+        class="flex flex-col border-b-0 sticky bg-white p-6
+        transition-all duration-200 ease-in-out shadow mb-6 rounded">
 
-    <div
-      ref="hidebar"
-      :style="search_offset"
-      v-if="!$route.query.hide_search"
-      class="flex flex-col border-b-0 sticky bg-white p-6
-      transition-all duration-200 ease-in-out shadow mb-6 rounded">
-
-      <div ref="search" class="flex flex-col">
-        <transition
-          name="fade-in-down"
-          mode="out-in"
-        >
-          <div
-            v-if="!all_businesses"
-            class="pb-4 flex-1 w-1/3 flex flex-col flex-grow"
-          >
-            <label
-              for="search-input"
-              class="text-base font-semibold m-0"
-            >help me find:</label>
-            <input
-              id="search-input"
-              class="border-0 border-b text-xl focus:border-gray-600
-              transition-colors focus:outline-none"
-              placeholder="Restaurants..."
-              v-model="search_query"
-            />
+          <div ref="search" class="flex flex-col">
+            <transition
+              name="fade-in-down"
+              mode="out-in"
+            >
+              <div
+                v-if="!all_businesses"
+                class="pb-4 flex-1 w-1/3 flex flex-col flex-grow"
+              >
+                <label
+                  for="search-input"
+                  class="text-base font-semibold m-0"
+                >help me find:</label>
+                <input
+                  id="search-input"
+                  class="border-0 border-b text-xl focus:border-gray-600
+                  transition-colors focus:outline-none"
+                  placeholder="Restaurants..."
+                  v-model="search_query"
+                />
+              </div>
+            </transition>
+            <div class="flex flex-row flex-wrap flex-shrink">
+              <ToggleSearch
+                class="mb-4 lg:mb-0"
+                text="all types of business"
+                :onchange="(v) => $set( this, 'all_businesses', v )"
+              />
+                <v-select
+                  class="mb-4 lg:mb-0"
+                  placeholder="Tribal affiliation (e.g. Osage)"
+                  v-model="search_affiliations"
+                  multiple
+                  :options="territories"
+                  label="name"
+                />
+                <v-select
+                  class="mb-4 lg:mb-0"
+                  placeholder="Tags (e.g. Food)"
+                  v-model="search_tags"
+                  multiple
+                  :options="tags"
+                  label="name"
+                />
+            </div>
           </div>
-        </transition>
-        <div class="flex flex-row flex-wrap flex-shrink">
-          <ToggleSearch
-            class="mb-4 lg:mb-0"
-            text="all types of business"
-            :onchange="(v) => $set( this, 'all_businesses', v )"
-          />
-            <v-select
-              class="mb-4 lg:mb-0"
-              placeholder="Tribal affiliation (e.g. Osage)"
-              v-model="search_affiliations"
-              multiple
-              :options="territories"
-              label="name"
-            />
-            <v-select
-              class="mb-4 lg:mb-0"
-              placeholder="Tags (e.g. Food)"
-              v-model="search_tags"
-              multiple
-              :options="tags"
-              label="name"
-            />
+          <transition name="fadee-in-down">
+            <button
+              v-if="is_scrolled_down"
+              @click="toggle_search"
+              class="border py-1 px-2 box-border text-md w-48"
+            >{{show_search ? 'hide' : 'show' }}</button>
+          </transition>
         </div>
-      </div>
-      <transition name="fadee-in-down">
-        <button
-          v-if="is_scrolled_down"
-          @click="toggle_search"
-          class="border py-1 px-2 box-border text-md w-48"
-        >{{show_search ? 'hide' : 'show' }}</button>
-      </transition>
-    </div>
-    <div class="font-bold text-lg whitespace-normal inline-block">
-      <transition name="fade-in-down">
-        <span v-if="minimum_search_criteria">Showing&nbsp;</span>
-      </transition>
-      <transition name="fade-in-down">
-        <span key="all" v-if="all_businesses" class="text-gray-500">
-          all businesses&nbsp;
-        </span>
-        <span v-else-if="search_query">
-          results for&nbsp;
-          <span class="text-gray-500">
-            "{{search_query}}"
+        <div class="font-bold text-lg whitespace-normal inline-block">
+          <transition name="fade-in-down">
+            <span v-if="minimum_search_criteria">Showing&nbsp;</span>
+          </transition>
+          <transition name="fade-in-down">
+            <span key="all" v-if="all_businesses" class="text-gray-500">
+              all businesses&nbsp;
+            </span>
+            <span v-else-if="search_query">
+              results for&nbsp;
+              <span class="text-gray-500">
+                "{{search_query}}"
+              </span>
+            </span>
+          </transition>
+          <span v-if="minimum_search_criteria && search_location">
+            in <span class="text-gray-500">{{search_location}}</span>&nbsp;
           </span>
-        </span>
-      </transition>
-      <span v-if="minimum_search_criteria && search_location">
-        in <span class="text-gray-500">{{search_location}}</span>&nbsp;
-      </span>
 
-      <!-- TODO(nsahler): i18n friendly -->
-      <span v-if="minimum_search_criteria && search_affiliations.length > 0">
-        &nbsp;affiliated with the&nbsp;
-        <span
-          v-for="(nation, i) in search_affiliations"
-          :key="i"
-        >
-          <span class="text-gray-500">{{nation.name}}</span>
-          <span v-if="i + 1 < search_affiliations.length - 1">
-            ,&nbsp;
+          <!-- TODO(nsahler): i18n friendly -->
+          <span v-if="minimum_search_criteria && search_affiliations.length > 0">
+            &nbsp;affiliated with the&nbsp;
+            <span
+              v-for="(nation, i) in search_affiliations"
+              :key="i"
+            >
+              <span class="text-gray-500">{{nation.name}}</span>
+              <span v-if="i + 1 < search_affiliations.length - 1">
+                ,&nbsp;
+              </span>
+              <span v-if="i + 1 === search_affiliations.length - 1">
+                &nbsp;or&nbsp;
+              </span>
+            </span>
+            <span>&nbsp;nation</span>
+            <span v-if="search_affiliations.length > 1">s</span>...
           </span>
-          <span v-if="i + 1 === search_affiliations.length - 1">
-            &nbsp;or&nbsp;
-          </span>
-        </span>
-        <span>&nbsp;nation</span>
-        <span v-if="search_affiliations.length > 1">s</span>...
-      </span>
+        </div>
+        <SearchResults v-if="show_search_results" :businesses="businesses" />
+        <transition name="fade-in-down">
+          <Loader v-if="minimum_search_criteria && !businesses"/>
+        </transition>
+      </main>
     </div>
-    <SearchResults v-if="show_search_results" :businesses="businesses" />
-    <transition name="fade-in-down">
-      <Loader v-if="minimum_search_criteria && !businesses"/>
-    </transition>
-  </div>
 </template>
 
 <script>
@@ -133,6 +114,7 @@ import 'vue-select/dist/vue-select.css'
 import ToggleSearch from '@/components/ToggleSearch'
 import SearchResults from '@/components/SearchResults'
 import Loader from '@/components/Loader'
+import Header from '@/components/Header'
 
 import {
   GET_BUSINESSES,
@@ -163,7 +145,8 @@ export default {
   components: {
     ToggleSearch,
     SearchResults,
-    Loader
+    Loader,
+    Header
   },
   methods: {
     toggle_search () {
