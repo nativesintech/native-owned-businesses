c4b973f4eb8910159b56c1dba39e60702820280d
Adam Recvlohe <adam.recvlohe@pm.me>
2021-03-27 11:31:37 -0500

    Update header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,12 @@
   >
     <header>
       <nav class="flex py-6 bg-white items-center justify-between">
-        <div class="flex">
+        <div class="flex items-center">
           <img
             src="@/assets/logo.svg" alt="Logo"
             class="flex-shrink-0 h-12 w-12"
           >
-          <h1 class="text-xl sm:text-xl lg:text-4xl text-gray-900 font-bold ml-3">
+          <h1 class="text-xl text-gray-900 font-bold ml-3">
             Native Owned Businesses
           </h1>
         </div>
